3ea530c321b05a2655c583d142c986cbcd6ade52
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-10 18:47:45 +0100

    js-elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// DOM elements
+const formElm = document.getElementById("form");
+
+// fields
+const titleFieldNameElm = document.getElementById("title-name");
+const fullNameFieldElm = document.getElementById("name-field");
+const kmFieldElm = document.getElementById("km-field");
+const ageFieldElm = document.getElementById("age-field");
+// card container 
